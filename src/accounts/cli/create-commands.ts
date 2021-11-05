007c3bdfd40a818ad54bcab2fcaff2f6f2034c92
slacktracer <slacktracer@gmail.com>
2021-11-05 17:39:26 -0300

    Changing the accounts/list command to accounts/read.

--- src/accounts/cli/create-commands.ts
+++ src/accounts/cli/create-commands.ts
@@ -3,7 +3,7 @@ import { readAccounts } from "../core/read-accounts.js";
 export const createCommands = ({ program }) => {
   const accountsCommand = program.command("accounts");
 
-  accountsCommand.command("list").action(async () => {
+  accountsCommand.command("read").action(async () => {
     const accounts = await readAccounts();
 
     console.table(accounts);
