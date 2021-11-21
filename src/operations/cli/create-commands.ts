6028bd085365e4b000b4e9b093f29d3d3c761bf8
slacktracer <slacktracer@gmail.com>
2021-11-21 18:49:00 -0300

    Update operation command and function placeholder.

--- src/operations/cli/create-commands.ts
+++ src/operations/cli/create-commands.ts
@@ -1,5 +1,6 @@
 import { createOperation } from "../core/create-operation/create-operation.js";
 import { readOperations } from "../core/read-operations.js";
+import { updateOperation } from "../core/update-operation.js";
 
 export const createCommands = ({ program }) => {
   const operationsCommand = program.command("operations");
@@ -24,6 +25,17 @@ export const createCommands = ({ program }) => {
 
     console.table(operations);
   });
+
+  operationsCommand
+    .command("update")
+    .requiredOption("-d, --data <data>")
+    .action(async (options) => {
+      const data = JSON.parse(options.data);
+
+      const operation = await updateOperation({ data });
+
+      console.table(operation);
+    });
 
   return operationsCommand;
 };
