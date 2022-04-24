cbda18c54a6fefc8e56033d31c55dcc9f5f71b2d
slacktracer <slacktracer@gmail.com>
2022-04-24 14:25:31 -0300

    SSL required to access remote database.

--- src/common/database.ts
+++ src/common/database.ts
@@ -2,6 +2,6 @@ import initPGPromise from "pg-promise";
 
 export const pgp = initPGPromise();
 
-export const db = pgp({});
+export const db = pgp({ ssl: true });
 
 export { loadQuery } from "./loadQuery.js";
