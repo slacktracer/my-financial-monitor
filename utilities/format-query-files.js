6ebe62fee7146f4dcda3161ad6911cf79fd11dd6
slacktracer <slacktracer@gmail.com>
2022-07-14 13:22:46 -0300

    PGP query files formatter.

---
+++ utilities/format-query-files.js
@@ -0,0 +1,42 @@
+import { promises as fs } from "fs";
+import { globby } from "globby";
+import { format } from "sql-formatter";
+
+const paths = await globby(["src/**/*.sql"]);
+
+const sqlFormatterOptions = {
+  keywordCase: "upper",
+  language: "postgresql",
+  tabWidth: 2,
+};
+
+const variablesMap = {};
+
+let variablesCount = 0;
+
+const formattingQueries = paths.map(async (path) => {
+  const query = await fs.readFile(path, "utf-8");
+
+  const queryWithPlaceholders = query.replace(
+    /\$\{ (.+?) \}/g,
+    (match, pcg1) => {
+      const key = `PLACEHOLDER_VARIABLE_${(variablesCount += 1)}`;
+      variablesMap[key] = pcg1;
+
+      return `$ { ${key} }`;
+    },
+  );
+
+  const formattedQuery = format(queryWithPlaceholders, sqlFormatterOptions);
+
+  const formattedQueryWithVariables = formattedQuery.replace(
+    /\$ \{ (.+?) \}/g,
+    (match, pcg1) => `$\{ ${variablesMap[pcg1]} }`,
+  );
+
+  return fs.writeFile(path, formattedQueryWithVariables + "\n");
+});
+
+Promise.all(formattingQueries).then(() => {
+  console.log("Done.");
+});
