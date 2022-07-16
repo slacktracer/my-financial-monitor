23329e6e46171d122520dafc77b6a2081285a17e
slacktracer <slacktracer@gmail.com>
2022-07-16 18:34:28 -0300

    Adds redis client to the database module/file.

--- src/common/database.ts
+++ src/common/database.ts
@@ -1,4 +1,5 @@
 import initPGPromise from "pg-promise";
+import { createClient } from "redis";
 
 export const pgp = initPGPromise();
 
@@ -7,3 +8,9 @@ const options = process.env.LOCAL === "yes" ? { ssl: true } : {};
 export const db = pgp(options);
 
 export { loadQuery } from "./load-query.js";
+
+const redisClient = createClient({ url: process.env.REDIS_CONNECTION_STRING });
+
+await redisClient.connect().catch(console.error);
+
+export { redisClient as kv };
