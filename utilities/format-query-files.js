12feedd634ef2e4266b2400fb107d451913e9230
slacktracer <slacktracer@gmail.com>
2022-07-24 18:46:45 -0300

    Refatored utility file/function. Now it looks at least decent.

--- utilities/format-query-files.js
+++ utilities/format-query-files.js
@@ -1,8 +1,8 @@
-import { promises as fs } from "node:fs";
 import { globby } from "globby";
+import { promises as fs } from "node:fs";
 import { format } from "sql-formatter";
 
-const paths = await globby(["../src/**/*.sql"]);
+const queryFilesPaths = await globby(["./src/**/*.sql"]);
 
 const sqlFormatterOptions = {
   keywordCase: "upper",
@@ -12,31 +12,67 @@ const sqlFormatterOptions = {
 
 const variablesMap = {};
 
-let variablesCount = 0;
+variablesMap.$count = 0;
 
-const formattingQueries = paths.map(async (path) => {
-  const query = await fs.readFile(path, "utf-8");
+const placePlaceHoldersReplaceFunction = (
+  match,
+  parenthesizedCaptureGroup1,
+) => {
+  variablesMap.$count += 1;
 
-  const queryWithPlaceholders = query.replace(
-    /\$\{ (.+?) \}/g,
-    (match, pcg1) => {
-      const key = `PLACEHOLDER_VARIABLE_${(variablesCount += 1)}`;
-      variablesMap[key] = pcg1;
+  const key = `PLACEHOLDER_VARIABLE_${variablesMap.$count}`;
 
-      return `$ { ${key} }`;
-    },
+  variablesMap[key] = parenthesizedCaptureGroup1;
+
+  return `$ { ${key} }`;
+};
+
+const placePlaceholders = ({ query }) => {
+  const queryVariableRegex = /\$\{ (.+?) \}/g;
+
+  return query.replace(queryVariableRegex, placePlaceHoldersReplaceFunction);
+};
+
+const placeVariablesBackReplaceFunction = (
+  match,
+  parenthesizedCaptureGroup1,
+) => {
+  if (parenthesizedCaptureGroup1 === "$count") {
+    throw new Error("Oh, no!");
+  }
+
+  return `$\{ ${variablesMap[parenthesizedCaptureGroup1]} }`;
+};
+
+const placeVariablesBack = ({ query }) => {
+  const placeholderRegex = /\$ \{ (.+?) \}/g;
+
+  return query.replace(placeholderRegex, placeVariablesBackReplaceFunction);
+};
+
+const formattingQueries = queryFilesPaths.map(async (queryFilesPath) => {
+  const query = await fs.readFile(queryFilesPath, "utf-8");
+
+  const queryWithPlaceholders = placePlaceholders({ query });
+
+  const formattedQueryWithPlaceholders = format(
+    queryWithPlaceholders,
+    sqlFormatterOptions,
   );
 
-  const formattedQuery = format(queryWithPlaceholders, sqlFormatterOptions);
+  const uppercasedFormattedQueryWithPlaceholders =
+    formattedQueryWithPlaceholders.toUpperCase();
 
-  const formattedQueryWithVariables = formattedQuery.replace(
-    /\$ \{ (.+?) \}/g,
-    (match, pcg1) => `$\{ ${variablesMap[pcg1]} }`,
+  const uppercasedFormattedQueryWithVariables = placeVariablesBack({
+    query: uppercasedFormattedQueryWithPlaceholders,
+  });
+
+  return fs.writeFile(
+    queryFilesPath,
+    uppercasedFormattedQueryWithVariables + "\n",
   );
-
-  return fs.writeFile(path, formattedQueryWithVariables + "\n");
 });
 
-Promise.all(formattingQueries).then(() => {
-  console.log("Done.");
-});
+Promise.all(formattingQueries).then(() =>
+  console.log("[FORMAT_QUERY_FILES] Done."),
+);
