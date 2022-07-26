4350d2e8438a669dc2122216aad3f7adb3c80ad2
slacktracer <slacktracer@gmail.com>
2022-07-26 18:05:38 -0300

    This should have been in the last commit. See it to know more!

--- src/application/database.ts
+++ src/application/database.ts
@@ -1,9 +1,10 @@
 import initPGPromise from "pg-promise";
 import { createClient } from "redis";
 
+import { transformColumnNames } from "./transform-column-names.js";
 import { loadInMemoryDatabase } from "./load-in-memory-database.js";
 
-export const pgp = initPGPromise();
+const pgp = initPGPromise();
 
 const options = process.env.LOCAL === "yes" ? { ssl: true } : {};
 
@@ -11,6 +12,8 @@ export const db =
   process.env.USE_MEMORY_DATABASE === "yes"
     ? await loadInMemoryDatabase()
     : pgp(options);
+
+db.$config.options.receive = transformColumnNames;
 
 export { loadQuery } from "./load-query.js";
 
